2a47156a2c0b7c7a9d28d4658e600fa6ef85cfaa
Tannu Ahuja <tannu.ahuja@knoldus.com>
2023-03-22 15:37:19 +0530

    Update and rename sum.java to Sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,6 @@
-    public class sum  
-    {  
-    public static void main(String args[])   
-    {  
+    public class Sum {
+      public static void main(String args[])   
+      {  
     int n1 = 225, n2 = 115, sum;  
     sum = n1 + n2;  
     System.out.println("The sum of numbers is: "+sum);  
